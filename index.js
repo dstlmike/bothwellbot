14ed81055708f676d94d5f3793cd7cb91f67dfc1
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-06 21:36:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ server = http.createServer(function (req, res) {
 });
 
 port = "8080"; // process.env.PORT; //Number(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 27017 || 8080 || 3002);
-ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+ip = "127.0.0.1"; //process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 server.listen(port, ip);
 
