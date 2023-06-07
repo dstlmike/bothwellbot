97316dd177f91e07d3348618897cacb4e25196c5
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-06 22:24:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ server = http.createServer(function (req, res) {
   });
 });
 
-port = Number( process.env.PORT || 27017 || 8080 || 3002);
-ip = process.env.IP || "0.0.0.0" || "127.0.0.1"; //process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+port = process.env.PORT || "27017" || "8080" || "3002";
+ip = "0.0.0.0" || "127.0.0.1"; //process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
-server.listen(ip);
+server.listen(port, ip);
 
 function ping() {
 //  console.log(this.res);
