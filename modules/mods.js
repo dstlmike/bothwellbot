bd728da848c0d4f6bdd86ba8f585889dbc43c307
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-07 02:35:16 -0400

    Update mods.js

--- modules/mods.js
+++ modules/mods.js
@@ -1,7 +1,7 @@
 var mods = [];
 var db = require('./db.js');
-var modCommands = [addModCmd, listModsCmd];
-
+var modCommands = [addModCmd, cmdConfig, listModsCmd];
+var owner = process.env.BOT_OWNER_ID;
 //init - make an init function
 db.getMods(function(res){
   mods = res;
@@ -65,6 +65,59 @@ function getModNames(){
   }
   return names;
 }
+
+function cmdConfig(request, currentBot, owner, callback) {
+
+  var regex = /^\/config (.+)/i;
+
+  var reqText = request.text;
+
+  if (regex.test(reqText)) {
+
+    var val = regex.exec(reqText);
+
+    if (rooms['config']){
+
+      callback(true, "You've already set a config ID. If you wish to reset it for some reason, you'll need to clear the database and start over.")
+
+      return true;
+
+    } else if (val[1].length != 26) {
+
+      callback(true, "That's not the right length for a Bot ID", []);
+
+      return true;
+
+    }
+
+    rooms['config'] = val[1];
+
+    addRoomToDB({
+
+      name: 'config',
+
+      id: val[1]
+
+    });
+
+    addConfigToDB({
+
+      config: 'owner',
+
+      id: request.user_id
+
+    });
+
+    var msg = 'Config Bot ID is set.\n\nYou will be recognized as the bot owner as well as a moderator.\n\nYou can add the bot to additional rooms.\n-First create the bot at dev.groupme.com, like you did the Config bot.\n-Change the end of the callback url form config to something different with no spaces.\n-This new end of the callback url needs to be the same as the name you pick for the add room command in the next step and should be unique to your other bots. It does not need to be the same name you chose for the name of the bot at dev.groupme.com, it just needs to be something you will recognize should you need to make changes.\n-Last use the /room add command as follows:\n/room add <name matching the end of the callback url> <Bot ID>\nEX: /room add foo dV82tx6bA6cstUZX7ghY7aho3y\n\nThere is a list of commands available by typing /commands.';
+
+    callback(true, msg, []);
+
+    return msg;
+
+  }
+
+}
+
 
 function addModCmd(request, dataHash, callback) {
   var regex = /^\/mod add (.+?) ([\s\S]+)/i;
