191e012afb271ae46294ed796dcddabeb908da6e
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-08 23:10:16 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -21,6 +21,7 @@ function connect(callback){
 function getAllDocuments(collection, callback) {
   mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
     if(err) throw err;
+    var collection = [];
     var allDocs = db.collection(collection).find().toArray(function(err, docs) {
       callback(docs);
       db.close();
