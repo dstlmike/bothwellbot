21f5e05284392c6c9671b13ccc64c251eb523f2f
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-09 02:46:29 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -10,6 +10,67 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   process.env.OPENSHIFT_APP_NAME;
 }
 
+const {MongoClient} = require('mongodb');
+
+main().catch(console.error);
+
+async function main(){
+
+    /**
+
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+
+     */
+
+    const uri = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+ 
+
+    const client = new MongoClient(uri);
+
+ 
+
+    try {
+
+        // Connect to the MongoDB cluster
+
+        await client.connect();
+
+ 
+
+        // Make the appropriate DB calls
+
+        await  listDatabases(client);
+
+ 
+
+    } catch (e) {
+
+        console.error(e);
+
+    } finally {
+
+      //  await client.close();
+
+    }
+
+}
+
+//main().catch(console.error);
+
+async function listDatabases(client){
+
+    databasesList = await client.db().admin().listDatabases();
+
+ 
+
+    console.log("Databases:");
+
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+
+};
 
 function connect(callback){
   mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
