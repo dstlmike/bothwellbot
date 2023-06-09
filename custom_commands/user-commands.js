3153ad3ad0ba8f395c3cb6d94305b87d5994a2c1
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-09 03:06:47 -0400

    Update user-commands.js

--- custom_commands/user-commands.js
+++ custom_commands/user-commands.js
@@ -10,6 +10,56 @@ main().catch(console.error);
 getAllCommands();
 exports.modName = "Custom Commands";
 
+const {MongoClient} = require('mongodb');
+
+main().catch(console.error);
+
+async function main(){
+
+    /**
+
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+
+     */
+
+    const uri = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+ 
+
+    const client = new MongoClient(uri);
+
+ 
+
+    try {
+
+        // Connect to the MongoDB cluster
+
+        await client.connect();
+
+ 
+
+        // Make the appropriate DB calls
+
+        await  listDatabases(client);
+
+ 
+
+    } catch (e) {
+
+        console.error(e);
+
+    } finally {
+
+      //  await client.close();
+
+    }
+
+}
+
+//main().catch(console.error);
+
 async function listDatabases(client){
 
     databasesList = await client.db().admin().listDatabases();
