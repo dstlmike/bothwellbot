0e3615a2e86cdb265f2caa66d6cbee343145773d
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-09 02:11:01 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -140,28 +140,3 @@ exports.findMod = function(mod, callback) {
     });
   });
 };
-async function main(){
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const uri = "mongodb+srv://alexbot:308boonave@<your-cluster-url>/testttt?retryWrites=true&w=majority";
- 
-
-    const client = new MongoClient(uri);
- 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
- 
-        // Make the appropriate DB calls
-        await  listDatabases(client);
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
-
-main().catch(console.error);
