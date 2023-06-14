34faa7d0cc74f7d98566fe5193f121169818e025
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-14 01:50:41 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -44,7 +44,7 @@ async function main(){
 
         await  listDatabases(client);
 await listCollections(client);
- await getAllDocuments(client);
+ await nocuments(client);
 
     } catch (e) {
 
@@ -104,10 +104,10 @@ function connect(callback){
   });
 }
 
-async function getAllDocuments(client) {
+async function nocuments(client) {
 //  mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
   //  if(err) throw err;
-    var ret = [];
+    //var ret = [];
  var dbo = await client.db().collection('config'); //.listCollections();
   dbo.find().forEach(dbo => console.log(` - ${dbo}`));
   //  var allDocs = db.collection(collection).find().toArray(function(err, docs) {
