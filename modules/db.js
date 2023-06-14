ca39e9b607feeeb511547e34fb9cc4dfde97df07
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-14 02:53:45 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -114,7 +114,7 @@ async function nocuments(client) {
     "config": dbo.config
     }
  var dbo = await client.db('sampledb').collection('rooms'); //.listCollections();
-  dbo.find().forEach(dbo => console.log(` - ${list}`));
+  dbo.find().forEach(dbo => console.log(` - ${dbo.list}`));
   //  var allDocs = db.collection(collection).find().toArray(function(err, docs) {
     //  callback(docs);
       //db.close();
