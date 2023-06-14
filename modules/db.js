23e5930b6195475346a058221ff3ce96ae33ee26
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-14 01:54:47 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -117,30 +117,17 @@ async function nocuments(client) {
  // });
 };
 
-exports.getAllDocuments = function(collection, docs, callback) {
-
-  mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
-
-
+function getAllDocuments(collection, callback) {
+  mongoDB.connect(connection_string, function(err, db) {
     if(err) throw err;
-var ret = [];
-    //var allDocs = db.collection(collection).find().toArray(function(err, docs) {
-
-    var allDocs = db.collection(collection).find().toArray(docs, function(err, result){
-
-      if (callback)
-
-        callback(result);
-
-     //db.close();
-
+    var allDocs = db.collection(collection).find().toArray(function(err, docs) {
+      callback(docs);
+      db.close();
     });
-
   });
+}
 
-}  
-
-//exports.getAllDocuments = getAllDocuments;
+exports.getAllDocuments = getAllDocuments;
 
 exports.addDoc = function(collection, doc, callback) {
   connect(function(db){
