39737e7a4d3ad5f79962e49ad151201a78ab22b6
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-16 01:38:16 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -10,6 +10,7 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   process.env.OPENSHIFT_APP_NAME;
 }
 //
+connect();
 const {MongoClient} = require('mongodb');
 
 main().catch(console.error);
@@ -101,6 +102,7 @@ function connect(callback){
   mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
     if(err) throw err;
     callback(db);
+    console.log(db);
   });
 }
 
