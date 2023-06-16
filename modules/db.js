2c3c4ff48e1ade5c380c6c2062b4863e018c4ad3
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-16 03:35:27 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -46,7 +46,7 @@ async function main(){
         await  listDatabases(client);
 await listCollections(client);
  await nocuments(client);
-await console.log(client.connect);
+await console.log(client());
     } catch (e) {
 
         console.error(e);
