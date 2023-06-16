841be1537232e562dcb86e5476bab9ce6515eb16
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-16 02:17:25 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -100,11 +100,44 @@ async function listCollections(client){
 };
 
 function connect(callback){
-  mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
-    if(err) throw err;
-    callback(db);
-    console.log('Test: ' + '\n' + db);
-  });
+ // mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
+   // if(err) throw err;
+   // callback(db);
+   // console.log('Test: ' + '\n' + db);
+ // });
+  const uri = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+ //
+
+    const client = new MongoClient(uri);
+
+ 
+
+    try {
+
+        // Connect to the MongoDB cluster
+
+        await client.connect();
+
+await callback(db);
+
+        // Make the appropriate DB calls
+
+       // await  listDatabases(client);
+
+//await listCollections(client);
+
+// await nocuments(client);
+
+    } catch (e) {
+
+        console.error(e);
+
+    } finally {
+
+      //  await client.close();
+
+    }
 }
 
 async function nocuments(client) {
