78171d0948abd1500e61d06aad783491f6827d59
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-16 00:23:45 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -9,7 +9,7 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
   process.env.OPENSHIFT_APP_NAME;
 }
-
+//
 const {MongoClient} = require('mongodb');
 
 main().catch(console.error);
