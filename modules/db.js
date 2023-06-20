1990d597f5e6f8f56de41cc386471aa257848b47
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-20 19:44:58 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -156,10 +156,10 @@ async function nocuments(client) {
 
   //  dbo.find().toArray(function(err, results) {;
 // return results;
- var dboo = dbo.find({}).toArray(function(res){;
+ var dboo = dbo.find({}).toArray(function(res){
    if (res) console.log(res); //.find().forEach(dbo => console.log(` Name: ${dbo.name}` + `\n` + ` Id: ${dbo.id}`));
 console.log('This.res');
-                                              )};                                   console.log(dbo); //JSON.stringify(dbo));
+                                              });                                   console.log(dbo); //JSON.stringify(dbo));
  //   db.close();
 //});
   };
