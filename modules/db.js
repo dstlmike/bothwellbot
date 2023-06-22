fb117f0e45e25b2d07bd36392c7a3f4d95e49032
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-22 17:21:08 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -105,6 +105,7 @@ var db_config   = require('../config/db-config.js');
 function connect(collection, callback){
  mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, results) {
     if(err) throw err;
+   
    callback(results);
    // callback(db);
     console.log('Test: ' + '\n' + results);
@@ -142,7 +143,7 @@ function connect(collection, callback){
       //  await client.close();
 
     }
-}
+});
 
 async function nocuments(client) {
 //  mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
