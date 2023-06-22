3d645aeb56cb56056261340d57023d8f3a976dd4
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-22 18:36:14 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -1,7 +1,7 @@
 var db_config   = require('../config/db-config.js');
 var mongoDB     = require('mongodb').MongoClient;
 var db_table = {}; //var db = require('mongodb').Db
-var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 //if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
 ////  connection_string;
 /*= 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
