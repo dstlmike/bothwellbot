ed7a010a57c8094b0dec627a015589d9a77e83d4
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-22 20:22:10 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -10,8 +10,8 @@ var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
   process.env.OPENSHIFT_APP_NAME;*/
 //}
-connect();
-getAllDocuments();
+//connect();
+//getAllDocuments();
 //connect(client); //.catch(console.error);
 const {MongoClient} = require('mongodb');
 
